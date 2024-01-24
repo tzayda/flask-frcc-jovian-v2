b7deee1ec2269b772a6443fe55aa5172fdc08fbf
tzayda <89423155+tzayda@users.noreply.github.com>
2024-01-24 10:06:39 +0000

    added connection to DB

--- app.py
+++ app.py
@@ -1,40 +1,21 @@
 from flask import Flask, render_template, jsonify
+from database import load_jobs_from_db
 
 app = Flask(__name__)
-
-JOBS = [{
-    'id': 1,
-    'title': 'Data Scientist',
-    'location': 'Bamberg, Germany',
-    'salary': '€ 5.000'
-}, {
-    'id': 2,
-    'title': 'Blockchain Engineer',
-    'location': 'Boston, USA',
-    'salary': '$ 6.000'
-}, {
-    'id': 3,
-    'title': 'Linux/DevOps Engineer',
-    'location': 'Remote',
-    'salary': '$ 7.000'
-}, {
-    'id': 4,
-    'title': 'Backend Developer',
-    'location': 'Nuremberg, Germany',
-    'salary': '€ 8.000 '
-}]
 
 
 #html route
 @app.route('/')
-def hello_world():
-  return render_template("home.html", jobs=JOBS, company_name="Tayril")
+def home_page():
+  jobs = load_jobs_from_db()
+  return render_template("home.html", jobs=jobs, company_name="Tayril")
 
 
 #api route
 @app.route('/api/jobs')
 def list_jobs():
-  return jsonify(JOBS)
+  jobs = load_jobs_from_db()
+  return jsonify(jobs)
 
 
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -1,17 +1,20 @@
+import os
 from sqlalchemy import create_engine, text
 
-db_connection_string = 'mysql+pymysql://qdt5tsr34q66pnpb50dm:pscale_pw_AZ77Y976rmcVBg55mXsBIGbvhviPZLQVed6i8GlViko@aws.connect.psdb.cloud/flaskcareers?charset=utf8mb4'
+
+db_connection_string = os.environ['DB_CONNECTION_STR']
 
 engine = create_engine(db_connection_string,
                        connect_args={"ssl": {
                            "ssl_ca": "/etc/ssl/cert.pem"
                        }})
 
-with engine.connect() as conn:
-  result = conn.execute(text("select * from jobs"))
 
-  result_dicts = []
-  for row in result.mappings().all():
-    result_dicts.append(dict(row))
-
-  print(result_dicts)
+#jobs'page
+def load_jobs_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs"))
+    jobs = []
+    for row in result.mappings().all():
+      jobs.append(dict(row))
+    return jobs
