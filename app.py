b3a62bd45b1c9d42ce949e05105a32a62439c7b2
tzayda <89423155+tzayda@users.noreply.github.com>
2024-01-25 15:09:21 +0000

    added html for application form and submitted applications

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
@@ -22,11 +22,20 @@ def list_jobs():
 @app.route('/job/<id>')
 def show_job(id):
   job = load_job_from_db(id)
-
   if not job:
     return "Not Found", 404
   else:
     return render_template("jobpage.html", job=job)
+
+
+#application route
+@app.route('/job/<id>/apply', methods=['POST'])
+def apply_job(id):
+  data = request.form
+  job = load_job_from_db(id)
+  return render_template("application_submitted.html",
+                         application=data,
+                         job=job)
 
 
 if __name__ == "__main__":
